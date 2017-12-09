5a8b1367ffcf9ee452225af67d3298001f9d6b3a
Radivis <radivis@radivis.com>
2017-12-09 13:07:00 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// Logger middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// Maintenance middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'We are broked',
+//     welcomeMessage: 'Thank you for having chosen the WRONG pill!'
+//   });
+// });
+
+// Static rendering middleware
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hello Express!</h1>');
+//   res.send({
+//     name: 'Michael',
+//     likes: [
+//       'Programming',
+//       'Biking',
+//       'Horses',
+//       'Writing'
+//     ]
+//   });
+// });
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Thank you for having chosen the red pill!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+// /bad - send back json with errorMessage
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorCode: 404,
+    errorMessage: 'Bad Request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Express.js HTTP server is up on port 3000');
+});
